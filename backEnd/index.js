d0b2b96f3c2bddc0760a1c2daca58de652aa825e
nongviettruong12 <nongviettruong12@gmail.com>
2024-10-18 14:08:41 +0700

    "feat":"refactor"

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const sequelize = require('./database');
-const itemRouter = require('./routes/itemRoutes')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const sequelize = require("./database");
+const itemRouter = require("./routes/itemRoutes");
 
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/api',itemRouter)
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", itemRouter);
 
-sequelize.sync().then(()=>{
+sequelize
+  .sync()
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}).catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
